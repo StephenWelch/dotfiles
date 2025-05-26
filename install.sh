cbc717a337535f55ba381531691c1243bbaf530e
Stephen Welch <7754730+StephenWelch@users.noreply.github.com>
2025-05-26 05:26:42 +0000

    Add uv install

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-sudo apt update && sudo apt install -y stow build-essential git neovim
+#!/bin/bash
+
+# Install dependencies
+sudo apt update && sudo apt install -y stow build-essential git neovim curl
+
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
 make
